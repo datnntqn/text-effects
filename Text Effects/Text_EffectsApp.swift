b956865ed6933c16cd2b6526be7589fc988a0815
DATNNT <thanhdat98qn@gmail.com>
2025-11-24 22:30:01 +0700

    init

---
+++ Text Effects/Text_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_EffectsApp.swift
+//  Text Effects
+//
+//  Created by DATNNT on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Text_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
